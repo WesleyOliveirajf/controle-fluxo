432ac699e90670b74b04809ac202c20d5e28bf69
Wesley Oliveira <139356104+WesleyOliveirajf@users.noreply.github.com>
2025-11-12 16:59:39 -0300

    first commit

---
+++ src/condicionalComposta.java
@@ -0,0 +1,13 @@
+public class condicionalComposta {
+    public static void main(String[] args) {
+        double nota = 7;
+
+        if (nota >= 7){
+            System.out.println("Sua nota e: " + nota + " ,voce esta aprovado.");
+        }else{
+            System.out.println(" voce esta reprovado");
+        }
+
+        }
+    }
+
